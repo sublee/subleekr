62b0a7467b9478ec7e16f53875cdfe100b683817
Lee Heung-sub <heung@sublee.kr>
2010-08-24 18:40:20 +0900

    Add `subleekr.app` and `.gitignore`.

---
+++ subleekr/app.py
@@ -0,0 +1,24 @@
+import os.path
+from flask import Flask
+from flaskext.sqlalchemy import SQLAlchemy
+
+
+CONFIG_FILEPATH = os.path.join(os.path.dirname(__file__), "../config.cfg")
+
+
+def auto_register_modules(app):
+    """Registers modules from :mod:`subleekr` to application."""
+    import subleekr
+    for modname in subleekr.__modules__:
+        __import__("{0}.{1}".format(subleekr.__name__, modname))
+        module = getattr(subleekr, modname)
+        module.app.super_app = app
+        app.register_module(module.app)
+
+
+app = Flask(__name__)
+if os.path.isfile(CONFIG_FILEPATH):
+    app.config.from_pyfile(CONFIG_FILEPATH)
+auto_register_modules(app)
+app.db = SQLAlchemy(app)
+
