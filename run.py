33684206015730f99fb95fc37ed042169bd9634d
Lee Heung-sub <heung@sublee.kr>
2010-08-25 17:32:35 +0900

    Change host to 127.0.0.1.

--- run.py
+++ run.py
@@ -1,11 +1,8 @@
-from meinheld import server
 from werkzeug.contrib.fixers import ProxyFix
 from subleekr.app import app
 
 
 if __name__ == "__main__":
-    #server.listen(("0.0.0.0", 8080))
-    #server.run(app)
     app.wsgi_app = ProxyFix(app.wsgi_app)
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="127.0.0.1", port=8080)
 
