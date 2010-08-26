b66b090fb3379fca530fe1cc06b98d3cdf2c43c8
Lee Heung-sub <heung@sublee.kr>
2010-08-26 11:59:00 +0900

    Implement initalization with setup script.

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os.path
+import re
 try:
     from setuptools import setup
 except ImportError:
@@ -17,10 +19,71 @@ def command(f):
 
 
 @command
+class initialize(distutils.cmd.Command):
+    """Initializes subleekr."""
+
+    CONFIG_FILEPATH = os.path.join(os.path.dirname(__file__), "config.cfg")
+    DEFAULT_CONFIG = {"SERVER_NAME": "sublee.kr",
+                      "SQLALCHEMY_DATABASE_URI": "sqlite:///subleekr/"
+                                                 "db.sqlite"}
+
+    user_options = [("server=", "s", "the server name"),
+                    ("db=", "d", "a connection string for the database"),
+                    ("createdb", None, "creates the database if not exists")]
+
+    def initialize_options(self):
+        self.server = None
+        self.db = None
+        self.createdb = False
+        self.same_config = False
+
+    def finalize_options(self):
+        if not self.server and not self.db:
+            self.same_config = True
+        config = {}
+        d = type(os.path)("config")
+        d.__file__ = self.CONFIG_FILEPATH
+        try:
+            execfile(d.__file__, d.__dict__)
+            for key in dir(d):
+                if key.isupper():
+                    config[key] = getattr(d, key)
+        except IOError:
+            config = self.DEFAULT_CONFIG
+        if not self.server:
+            self.server = config["SERVER_NAME"]
+        if not self.db:
+            self.db = config["SQLALCHEMY_DATABASE_URI"]
+
+    def run(self):
+        if not self.same_config:
+            if os.path.isfile(self.CONFIG_FILEPATH):
+                replace = raw_input("config file already exists. "
+                                    "do you want ro replace it? "
+                                    "[Y/n]").lower() == "y"
+                if not replace:
+                    log.info("initialization is canceled.")
+                    return
+            with open(self.CONFIG_FILEPATH, "w") as config:
+                blank = re.compile("^\s*", re.MULTILINE)
+                config_code = re.sub(blank, "", """
+                    SERVER_NAME = {server!r}
+                    SQLALCHEMY_DATABASE_URI = {db!r}
+                """.format(server=self.server, db=self.db))
+                config.write(config_code)
+                del config
+        if self.createdb:
+            from subleekr.app import app
+            log.info("create the database...")
+            app.db.create_all()
+        log.info("initialization has completed successfully.")
+
+
+@command
 class adduser(distutils.cmd.Command):
     """Adds a new user into auth file."""
 
-    user_options = [("name=", "n", "the name of user."),
+    user_options = [("name=", "n", "the name of user"),
                     ("password=", "p", "the password of user")]
 
     def initialize_options(self):
