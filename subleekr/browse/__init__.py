26be6acca723ea4ff70bbe09c3c8c5586ec92ffb
Lee Heung-sub <heung@sublee.kr>
2010-09-01 17:14:25 +0900

    Remove `subleekr.browse`.

--- subleekr/browse/__init__.py
+++
@@ -1,9 +0,0 @@
-import os.path
-from flask import Module
-from flaskext.autoindex import AutoIndexModule
-
-
-browse_root = os.path.join(os.path.expanduser("~"), "lab")
-app = Module(__name__, subdomain="browse")
-AutoIndexModule(app, browse_root)
-
