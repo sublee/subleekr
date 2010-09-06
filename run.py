09d25f660f05bef6b5d704cc341e28d2a98e1136
Lee Heung-sub <heung@sublee.kr>
2010-09-06 17:38:34 +0900

    Make to `run.py` uses `create_app` function.

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from werkzeug.contrib.fixers import ProxyFix
-from subleekr.app import app
+from subleekr.app import create_app
 
 
 if __name__ == "__main__":
+    app = create_app()
     app.wsgi_app = ProxyFix(app.wsgi_app)
     app.run(host="127.0.0.1", port=8080)
 
