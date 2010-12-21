33f4dc8a639be52117c0cd94f47fe7c19450ce66
Heungsub Lee <heung@sublee.kr>
2010-12-21 11:44:06 +0900

    Add Cobb's totem animation

--- subleekr/limbo/__init__.py
+++ subleekr/limbo/__init__.py
@@ -1,5 +1,10 @@
+from flask import render_template
 from subleekr import FlaskModule
 
 
 app = FlaskModule(__name__, subdomain="limbo")
 
+
+@app.route("/")
+def index():
+    return render_template("limbo/index.html")
